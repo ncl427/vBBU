1080a6f3fb2b3d547d478d1c7746bb4fda3169bd
Muhammad Afaq <afaq24@gmail.com>
2017-12-06 18:49:36 +0900

    logging-work of vBBU

--- vBBU_client.py
+++ vBBU_client.py
@@ -1,14 +1,16 @@
 from bjsonrpc import connect
 
-def attachvBBU(ueIp, serviceType):
+def attachNSSF(ueIp, serviceType):
    nssfIp = "117.17.102.21"
    nssfPort = 10123
 
+   print "\n--------------------------"
    print "...connecting to vBBU-NSSF"
-   c = connect(nssfIp)
+   #c = connect(nssfIp)
    print "connected vBBU-NSSF."
 
-   print "attaching vBBU-NSSF: ", ueIp, " - ", serviceType
-   response =  c.call.networkAttach(ueIp, serviceType)
+   print "...attaching vBBU-NSSF: ", ueIp, " - ", serviceType
+   #response =  c.call.networkAttach(ueIp, serviceType)
    response = "attached vBBU-NSSF"
    print "attaching vBBU-NSSF response: ", response
+   return response
--- vBBU_server.py
+++ vBBU_server.py
@@ -4,9 +4,9 @@ from bjsonrpc import createserver
 
 # class for server handlers
 class ServerHandler(BaseHandler):
+
    def attachvBBU(self, ueIp, serviceType):
       return attachNSSF(ueIp, serviceType)
-      # return "abc"
 
 # creating the server to listen 
 s = createserver(host="192.168.186.61", port=10123, handler_factory=ServerHandler)
